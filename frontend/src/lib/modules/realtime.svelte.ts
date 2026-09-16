496242e3ef6afe59a810f3b99ac89d70c123f6ef
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-09-16 02:54:52 -0300

    Added realtime manager
    If they are so tightly coupled to one another, who am I to keep them separated? This is what I call true love.

--- frontend/src/lib/modules/realtime.svelte.ts
+++ frontend/src/lib/modules/realtime.svelte.ts
@@ -1,10 +1,39 @@
 import type { Board } from '$types/api';
 
 import { io } from 'socket.io-client';
+import { type DataConnection, Peer } from 'peerjs';
+
+import { mouses, type ClientData } from '$stores/mouses.svelte';
+
+import { SvelteMap } from 'svelte/reactivity';
 
 export type Update = { board: Board; ts: Date };
 
-export async function connect(board: string, peer: string, update: (data: Update) => void) {
+const colors = ['#FF0000', '#F0F000', '#00FF00', '#00F0F0', '#0000FF', '#F000F0'];
+
+export async function connect(board: string, update: (data: Update) => void) {
+	const id = crypto.randomUUID();
+
+	const peers = await socket(board, id, update);
+	const peer = rtc(id, peers);
+
+	return () => {
+		peer.destroy();
+		connections.clear();
+		mouses.clear();
+	};
+}
+
+const connections = new SvelteMap<string, DataConnection>();
+
+export function update(data: [number, number]) {
+	connections
+		.values()
+		.filter((c) => c.open)
+		.forEach((c) => c.send(data));
+}
+
+async function socket(board: string, peer: string, update: (data: Update) => void) {
 	const socket = io('http://localhost:3000', {
 		path: '/ws',
 		transports: ['websocket'],
@@ -22,4 +51,58 @@ export async function connect(board: string, peer: string, update: (data: Update
 	socket.on('update', update);
 
 	return peers;
+}
+
+function rtc(id: string, peers: string[]) {
+	const peer = new Peer(id);
+	const color = colors[peers.length % colors.length];
+
+	peer.on('connection', setPeerConnection);
+	peer.on('error', console.error);
+
+	peer.on('open', () =>
+		peers.forEach((p) => {
+			const conn = peer.connect(p, {
+				reliable: true,
+				metadata: {
+					username: 'Messi',
+					picture: 'TBD',
+					color
+				} satisfies ClientData
+			});
+
+			setPeerConnection(conn);
+		})
+	);
+
+	return peer;
+}
+
+function setPeerConnection(conn: DataConnection) {
+	const id = conn.peer;
+
+	conn.on('open', () => {
+		connections.set(id, conn);
+		mouses.add(id, conn.metadata);
+
+		conn.on('data', (data) => {
+			if (Array.isArray(data) && isNumber(data[0]) && isNumber(data[1])) {
+				const pos = { x: data[0], y: data[1] };
+				mouses.update(id, pos);
+			}
+		});
+	});
+
+	conn.on('close', () => {
+		connections.delete(id);
+		mouses.remove(id);
+
+		console.log('Lost', id);
+	});
+
+	conn.on('error', console.error);
+}
+
+function isNumber(n: unknown): n is number {
+	return Number.isFinite(n);
 }
--- frontend/src/routes/board/[id]/+page.svelte
+++ frontend/src/routes/board/[id]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import './index.css';
 
+	import type { Update } from '$modules/realtime.svelte.js';
+
 	import Flow from './Flow.svelte';
 	import DnDProvider from './DnDProvider.svelte';
 	import MouseTracker from './MouseTracker.svelte';
@@ -16,10 +18,16 @@
 		edges: Edge[];
 		name: string;
 	};
+
+	function boardUpdate(update: Update) {
+		data.nodes = update.board.postits;
+		data.edges = update.board.strands;
+		data.name = update.board.name;
+	}
 </script>
 
 <DnDProvider>
-	<MouseTracker boardId={id}>
+	<MouseTracker boardId={id} {boardUpdate}>
 		<Flow {name} {nodes} {edges} boardId={id} />
 	</MouseTracker>
 </DnDProvider>
