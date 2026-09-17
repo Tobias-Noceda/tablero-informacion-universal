dc9c8b4c4d92c9cb17d91a8268907a08966a2dc9
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-09-16 23:38:52 -0300

    Fix socket uri to exclude current path

--- frontend/src/lib/modules/sockets.svelte.ts
+++ frontend/src/lib/modules/sockets.svelte.ts
@@ -6,7 +6,7 @@ export type Update = { board: Board; ts: Date };
 
 export async function socket(board: string, peer: string, update: (data: Update) => void) {
 	const socket = io(
-		import.meta.env.VITE_API_URL || window?.location.href || 'http://localhost:62113',
+		import.meta.env.VITE_API_URL || window?.location.origin || 'http://localhost:62113',
 		{
 			path: '/ws',
 			transports: ['websocket'],
