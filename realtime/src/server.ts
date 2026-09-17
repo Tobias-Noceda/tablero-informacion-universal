f9c057ea229ee4a180923edd26077e14816dc5a4
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-09-17 03:20:55 -0300

    Modularize Mongo
    Now THIS is clean code. Look at that main, with just 5 lines of real code.
    
    I love JS so much...

--- realtime/src/server.ts
+++ realtime/src/server.ts
@@ -1,35 +1,15 @@
 import * as socket from "./socket.js";
-
-import mongo from "./mongo.js";
-
-const docs = await mongo.connect();
-
-const boards = docs.db("prod").collection("boards");
-const stream = boards.watch([{ $match: { operationType: "update" } }], {
-    fullDocument: "updateLookup",
-});
-
-stream
-    .on("change", (event) => {
-        const change = event as typeof event & { operationType: "update" };
-
-        const board = change.fullDocument;
-        const id = change.documentKey._id;
-
-        if (!board) return;
-
-        socket.notify(id.toString(), board);
-    })
-    .once("error", console.error);
+import * as mongo from "./mongo.js";
 
 if (import.meta.main) {
+    mongo.setStreamCallback(socket.notify);
+
     const port = process.env.PORT ?? 3000;
     socket.server.listen(port);
-}
 
-process.on("SIGTERM", async () => {
-    await stream.close();
-    await Promise.allSettled([socket, docs].map((r) => r.close()));
-});
+    process.on("SIGTERM", () =>
+        Promise.allSettled([socket.close(), mongo.close()]),
+    );
+}
 
 export default socket.server;
