349de43502781b58c55702f94487f43e7f638fc8
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-09-19 18:44:42 -0300

    feat(frontend): NASA picture of the day card

---
+++ frontend/src/lib/components/Nodes/NasaApod/NasaApod.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { type NodeProps } from '@xyflow/svelte';
+	import NodeWrapper from '../NodeWrapper.svelte';
+
+	let props: NodeProps = $props();
+</script>
+
+<NodeWrapper {...props}>
+	{#snippet children(data)}
+		<h3>{data.title}</h3>
+		{#if data.image}
+			<img src={data.image} alt={data.title} />
+		{/if}
+		<p>{data.date}</p>
+	{/snippet}
+</NodeWrapper>
+
+<style>
+	img {
+		max-width: 100%;
+		border-radius: 4px;
+	}
+</style>
