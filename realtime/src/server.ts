3ddfea3190816c7043f150ccc901bae15795c2bf
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-09-20 22:01:50 -0300

    I'm desperate

--- realtime/src/server.ts
+++ realtime/src/server.ts
@@ -1,15 +1,15 @@
 import * as socket from "./socket.js";
 import * as mongo from "./mongo.js";
 
-if (import.meta.main) {
-    mongo.setStreamCallback(socket.notify);
+mongo.setStreamCallback(socket.notify);
 
+if (import.meta.main) {
     const port = process.env.PORT ?? 3000;
     socket.server.listen(port);
-
-    process.on("SIGTERM", () =>
-        Promise.allSettled([socket.close(), mongo.close()]),
-    );
 }
+
+process.on("SIGTERM", () =>
+    Promise.allSettled([socket.close(), mongo.close()]),
+);
 
 export default socket.server;
